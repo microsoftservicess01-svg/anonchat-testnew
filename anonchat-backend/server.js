ed40c8cd835061771d6afed4fbec61ac5c172fc5
microsoftservicess01-svg <microsoftservicess01@gmail.com>
2025-11-08 23:29:27 +0530

    Update server.js

--- anonchat-backend/server.js
+++ anonchat-backend/server.js
@@ -1,40 +1,24 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-});
-
-let waitingUser = null;
+let users = {}; // userId -> socketId
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  socket.on("register", (userId) => {
+    users[userId] = socket.id;
+    socket.emit("registered", { userId });
+  });
 
-  if (waitingUser) {
-    const partnerId = waitingUser;
-    io.to(socket.id).emit("partner-found", partnerId);
-    io.to(partnerId).emit("partner-found", socket.id);
-    waitingUser = null;
-  } else {
-    waitingUser = socket.id;
-    socket.emit("waiting", "Looking for a partner...");
-  }
-
-  socket.on("offer", (data) => io.to(data.to).emit("offer", data));
-  socket.on("answer", (data) => io.to(data.to).emit("answer", data));
-  socket.on("ice-candidate", (data) => io.to(data.to).emit("ice-candidate", data));
+  socket.on("connect-user", (targetId) => {
+    const targetSocket = users[targetId];
+    if (targetSocket) {
+      io.to(targetSocket).emit("partner-found", socket.id);
+      io.to(socket.id).emit("partner-found", targetSocket);
+    } else {
+      socket.emit("user-not-found");
+    }
+  });
 
   socket.on("disconnect", () => {
-    if (waitingUser === socket.id) waitingUser = null;
-    console.log("User disconnected:", socket.id);
+    for (const [id, sId] of Object.entries(users)) {
+      if (sId === socket.id) delete users[id];
+    }
   });
 });
-
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
