c0dd397d948946a3fd0a4dc8b03d5f48b0d31c6b
microsoftservicess01-svg <microsoftservicess01@gmail.com>
2025-11-08 23:32:07 +0530

    Update React.js

--- anonchat-frontend/React.js
+++ anonchat-frontend/React.js
@@ -1,1 +1,6 @@
-
+function connectById() {
+  const myId = prompt("Enter your ID");
+  const friendId = prompt("Enter your friend's ID");
+  socket.emit("register", myId);
+  socket.emit("connect-user", friendId);
+}
